1a53d59f8044055e7e807c5657e8e1f40be01201
ishashukla16 <ishukla408@gmail.com>
2021-01-28 15:34:58 +0530

    l

--- accessmodifier/src/com/capgemini/access/First.java
+++ accessmodifier/src/com/capgemini/access/First.java
@@ -11,7 +11,7 @@ public class First extends For {
 	void methodd()
 	{
 		System.out.println("hey");
-		
+		System.out.println("hey2");
 	}
 public static void main(String[] args) {
 	First f=new First();
